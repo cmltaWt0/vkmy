6a13a41ea66d9ea6c48b405e551eaf33d4e5df88
Maksim Sokolski <misokolsky@gmail.com>
2013-08-18 23:05:34 +0300

    Refactor.

--- vkmy.py
+++ vkmy.py
@@ -2,23 +2,13 @@
 # -*- coding: utf-8 -*-
 
 import vkontakte
-import sys
-import ConfigParser
+from ConfigParser import ConfigParser
 
-def config():
-    conf = {}
-    config = ConfigParser.ConfigParser()
-    config.read('conf.cfg')
+config = ConfigParser()
+config.read('conf.cfg')
 
-    conf ['app_id'] = config.get('app', 'app_id')
-    conf ['app_secret'] = config.get('app', 'app_secret')
-
-    return conf
-
-conf = config()
-
-app_id = conf['app_id']
-app_secret = conf['app_secret']
+app_id = config.get('app', 'app_id')
+app_secret = config.get('app', 'app_secret')
 
 vk = vkontakte.API(app_id, app_secret)
 print vk.getServerTime()
